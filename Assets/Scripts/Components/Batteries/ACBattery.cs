be84331b35a70c8c80e26b4251acca83cc2723f3
Atharvap14 <77426939+Atharvap14@users.noreply.github.com>
2021-04-22 16:44:17 +0530

    ACbattery

--- Assets/Scripts/Components/Batteries/ACBattery.cs
+++ Assets/Scripts/Components/Batteries/ACBattery.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SharpCircuit;
 
 public class ACBattery : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public ACVoltageSource ACVolt;
+    void Awake()
+    {// instantiating 2 lead AC voltage source
+        ACVolt=CIrcuitSim.sim.Create<ACVoltageSource>();
     }
 
     // Update is called once per frame
