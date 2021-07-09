627367af770903973a0052884fa4e5cf134c0481
Azad Kshitij <azadkshitij08302001@gmail.com>
2021-07-09 21:08:27 +0530

    Mostly working

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public void ChangeValue(string value)
+    {
+        ComponentInitialization component = CircuitManager.selected.GetComponent<ComponentInitialization>();
+        if (component.tag != "Wire")
+        {
+            component.value = value;
+            component.valueText.text = value;
+        }
+
+    }
+}
