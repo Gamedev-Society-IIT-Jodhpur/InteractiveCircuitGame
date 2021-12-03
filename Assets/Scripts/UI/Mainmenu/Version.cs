f7919b121a9eafba0e36124fc7a7d7f306bb0712
Azad Kshitij <azadkshitij08302001@gmail.com>
2021-12-03 15:03:30 +0530

    Main Menu Working

---
+++ Assets/Scripts/UI/Mainmenu/Version.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Version : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<TMP_Text>().text = PlayerPrefs.GetString("email");
+    }
+}
