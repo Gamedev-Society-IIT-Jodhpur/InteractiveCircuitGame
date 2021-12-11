524fe6b7512a8ef77f7535511de048d554e3dde6
Atharvap14 <77426939+Atharvap14@users.noreply.github.com>
2021-12-11 14:23:37 +0530

    ErrorsResolved

--- Assets/Scripts/Managers/ValidateScript.cs
+++ Assets/Scripts/Managers/ValidateScript.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using SpiceSharp;
+using SpiceSharp.Components;
 
 public class ValidateScript : MonoBehaviour
 {
